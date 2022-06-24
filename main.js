64b0202e9a084353f189541d8db18d43349f9780
Olga Shikunova <88195636+Olshyk@users.noreply.github.com>
2022-06-24 09:01:12 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ function search() {
 
   const input = createElement('input' ,'search__input');
   searchWrapper.append(input);
-  const sugBox = createElement('ul' ,'search__autocomplete');
-  searchWrapper.append(sugBox);
+  const suggestionBox = createElement('ul' ,'search__autocomplete');
+  searchWrapper.append(suggestionBox);
   const list = createElement('ul' ,'list');
 
   app.append(searchWrapper);
   app.append(list);
 
-  function createElement(el, elClass) {
-    const element = document.createElement(el);
-    if (elClass) {
-      element.classList.add(elClass);
+  function createElement(tag, elementClass) {
+    const element = document.createElement(tag);
+    if (elementClass) {
+      element.classList.add(elementClass);
     }
     return element;
   }
@@ -28,40 +28,50 @@ function search() {
     }
   };
 
-  async function searchRepo() {
+  async function searchRepository() {
     if (input.value) {
-      sugBox.innerHTML = '';
+      suggestionBox.innerHTML = '';
       let url = `https://api.github.com/search/repositories?q=${input.value}&sort=stars&per_page=5&order=desc&page=1`;
-      return await fetch(url).then(res => {
-        if (res.ok) {
-          res.json().then(result => {
+      return await fetch(url).then(response => {
+        if (response.ok) {
+          response.json().then(result => {
             result.items.forEach(item => {
               let suggestion = createElement('li', 'search__suggestion');
               suggestion.textContent = item.name;
-              sugBox.append(suggestion);
-              suggestion.addEventListener('click', (e) => {
-                let repo = createElement('li', 'list__item');
-                repo.innerHTML = `<div>
+              suggestionBox.append(suggestion);
+
+              function addRepository () {
+                let repository = createElement('li', 'list__item');
+                repository.innerHTML = `<div>
                                     <p>Name: ${item.name}</p>
                                     <p>Owner: ${item.owner.login}</p>
                                     <p>Stars: ${item.stargazers_count}</p>
                                   </div>
                                   <button class="button" title="close"></button>`
-                list.append(repo);
-                sugBox.innerHTML = '';
-                const btn = repo.querySelector('.button');
-                btn.addEventListener('click', () => list.removeChild(repo));
-              })
+                list.append(repository);
+                suggestionBox.innerHTML = '';
+                const button = repository.querySelector('.button');
+
+                function clearList() {
+                  list.removeChild(repository);
+                  this.removeEventListener('click', clearList);
+                }
+
+                button.addEventListener('click', clearList);
+                this.removeEventListener('click', addRepository);
+              } 
+
+              suggestion.addEventListener('click', addRepository);
             })
           })
         } 
       })
     } else {
-      sugBox.innerHTML = '';
+      suggestionBox.innerHTML = '';
     }
   }
 
-  input.addEventListener('keyup', debounce(searchRepo, 1000));
+  input.addEventListener('input', debounce(searchRepository, 1000));
 }
 
 
